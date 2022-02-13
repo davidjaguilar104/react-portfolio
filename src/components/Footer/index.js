1284c156262f8bd274061932cf1dbcee58cc88fd
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-13 13:30:24 -0600

    basic footer in/need to change twitter link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,40 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer>
-      <div>
-        <h3>Hello World</h3>
+      <div className="links-wrapper">
+        <h3>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/davidjaguilar104"
+            alt="Link to GitHub profile"
+          >
+            GitHub
+          </a>
+        </h3>
+        <h3>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/david-aguilar-165bbb21b/"
+            alt="Link to LinkedIn profile"
+          >
+            LinkedIn
+          </a>
+        </h3>
+        <h3>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/david-aguilar-165bbb21b/"
+            alt="Link to LinkedIn profile"
+          >
+            Twitter
+          </a>
+        </h3>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
