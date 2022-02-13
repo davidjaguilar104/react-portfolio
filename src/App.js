112828d488850ae19db3cccbf17c3e6b61c6a527
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-13 13:54:48 -0600

    basic contact component built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from './pages/About/About';
+// import About from './pages/About/About';
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
     <main>
       <Header></Header>
-      <About></About>
+      <Contact></Contact>
       <Footer></Footer>
     </main>
   );
