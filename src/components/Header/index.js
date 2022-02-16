b451f1ff301d011f3f9300f6864f02af3674fb82
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-16 13:07:04 -0600

    added onClick to David Aguilar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,28 +5,30 @@ function Header({ currentPage, handlePageChange }) {
   return (
     <nav>
       <h1 id="david-a">
-        <a href="/">David Aguilar</a>
+        <a onClick={() => handlePageChange("#about")} href="#about">
+          David Aguilar
+        </a>
       </h1>
       <div className="nav-items-wrapper">
         <h3>
-          <a 
-          onClick={() => handlePageChange('#about')}
-          href="#about">About Me</a>
+          <a onClick={() => handlePageChange("#about")} href="#about">
+            About Me
+          </a>
         </h3>
         <h3>
-          <a 
-          onClick={() => handlePageChange('#portfolio')}
-          href="#portfolio">Portfolio</a>
+          <a onClick={() => handlePageChange("#portfolio")} href="#portfolio">
+            Portfolio
+          </a>
         </h3>
         <h3>
-          <a 
-          onClick={() => handlePageChange('#contact')}
-          href="#contact">Contact</a>
+          <a onClick={() => handlePageChange("#contact")} href="#contact">
+            Contact
+          </a>
         </h3>
         <h3>
-          <a 
-          onClick={() => handlePageChange('#resume')}
-          href="#resume">Resume</a>
+          <a onClick={() => handlePageChange("#resume")} href="#resume">
+            Resume
+          </a>
         </h3>
       </div>
     </nav>
