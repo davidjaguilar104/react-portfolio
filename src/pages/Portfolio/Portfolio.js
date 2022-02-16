0f597e744887210c34e4ee5950884466f2021c58
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-16 13:27:08 -0600

    portfolio page with links and img done

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -3,6 +3,9 @@ import "./Portfolio.css";
 import animalShowdown from "../../assets/background.jpg";
 import recipeNutrition from "../../assets/stew-brew.png";
 import budgetTracker from "../../assets/budget-tracker.png";
+import weatherDash from "../../assets/weather-dashboard.png";
+import noteTaker from "../../assets/note-taker.png";
+import passGen from "../../assets/pass-gen.png";
 
 function Portfolio() {
   return (
@@ -30,12 +33,12 @@ function Portfolio() {
           </a>
           <a
             href="https://davidjaguilar104.github.io/recipe-nutrition-app/"
-            alt="Link for Animal Showdown application"
+            alt="Link for Recipe Nutrition application"
           >
             <img
               className="app-img"
               src={recipeNutrition}
-              alt="Animal Showdown app"
+              alt="Recipe Nutrition app"
             ></img>
           </a>
         </div>
@@ -57,49 +60,45 @@ function Portfolio() {
           </a>
         </div>
         <div className="link-img">
-          <a href="https://github.com/gstockha/animal-showdown">
-            Animal Showdown Repository
+          <a href="https://github.com/davidjaguilar104/weather-dashboard">
+            Weather Dashboard Repository
           </a>
           <a
-            href="https://animalshowdown.herokuapp.com/login"
-            alt="Link for Animal Showdown application"
+            href="https://davidjaguilar104.github.io/weather-dashboard/"
+            alt="Link for Weather Dashboard application"
           >
             <img
               className="app-img"
-              src={animalShowdown}
-              alt="Animal Showdown app"
+              src={weatherDash}
+              alt="Weather Dashboard app"
             ></img>
           </a>
         </div>
       </div>
       <div className="app-row">
         <div className="link-img">
-          <a href="https://github.com/gstockha/animal-showdown">
-            Animal Showdown Repository
+          <a href="https://github.com/davidjaguilar104/note-taker">
+            Note Taker Repository
           </a>
           <a
-            href="https://animalshowdown.herokuapp.com/login"
-            alt="Link for Animal Showdown application"
+            href="https://glacial-coast-25788.herokuapp.com/"
+            alt="Link for Note Taker application"
           >
-            <img
-              className="app-img"
-              src={animalShowdown}
-              alt="Animal Showdown app"
-            ></img>
+            <img className="app-img" src={noteTaker} alt="Note Taker app"></img>
           </a>
         </div>
         <div className="link-img">
-          <a href="https://github.com/gstockha/animal-showdown">
-            Animal Showdown Repository
+          <a href="https://github.com/davidjaguilar104/password-generator">
+            Password Generator Repository
           </a>
           <a
-            href="https://animalshowdown.herokuapp.com/login"
-            alt="Link for Animal Showdown application"
+            href="https://davidjaguilar104.github.io/password-generator/"
+            alt="Link for Password Generator application"
           >
             <img
               className="app-img"
-              src={animalShowdown}
-              alt="Animal Showdown app"
+              src={passGen}
+              alt="Password Generator app"
             ></img>
           </a>
         </div>
