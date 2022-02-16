75c50d83eb764dd4853b99cbc58d86ea97e79f6e
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-16 13:51:48 -0600

    added nav link highlight on selected

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,28 +5,28 @@ function Header({ currentPage, handlePageChange }) {
   return (
     <nav>
       <h1 id="david-a">
-        <a onClick={() => handlePageChange("#about")} href="#about">
+        <a className={currentPage === '#about' && 'nav-link'} onClick={() => handlePageChange("#about")} href="#about">
           David Aguilar
         </a>
       </h1>
       <div className="nav-items-wrapper">
         <h3>
-          <a onClick={() => handlePageChange("#about")} href="#about">
+          <a className={currentPage === '#about' && 'nav-link'} onClick={() => handlePageChange("#about")} href="#about">
             About Me
           </a>
         </h3>
         <h3>
-          <a onClick={() => handlePageChange("#portfolio")} href="#portfolio">
+          <a className={currentPage === '#portfolio' && 'nav-link'} onClick={() => handlePageChange("#portfolio")} href="#portfolio">
             Portfolio
           </a>
         </h3>
         <h3>
-          <a onClick={() => handlePageChange("#contact")} href="#contact">
+          <a className={currentPage === '#contact' && 'nav-link'} onClick={() => handlePageChange("#contact")} href="#contact">
             Contact
           </a>
         </h3>
         <h3>
-          <a onClick={() => handlePageChange("#resume")} href="#resume">
+          <a className={currentPage === "#resume" && 'nav-link'} onClick={() => handlePageChange("#resume")} href="#resume">
             Resume
           </a>
         </h3>
