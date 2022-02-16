4756fd758530340f4c4cd97c31d54216a1d68ee8
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-16 13:43:55 -0600

    added proficiencies

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -5,6 +5,28 @@ import myResume from "../../assets/Tech Resume.docx";
 function Resume() {
   return (
     <section>
+      <h3>Proficient in: </h3>
+      <div className="tech-wrapper">
+        <ul>
+          <h4>Frontend</h4>
+          <li>React</li>
+          <li>Angular</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Bootstrap</li>
+          <li>JQuery</li>
+        </ul>
+        <ul>
+          <h4>Backend</h4>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>MySQL</li>
+          <li>Sequlize</li>
+          <li>MongoDB</li>
+          <li>Mongoose</li>
+        </ul>
+      </div>
       <button>
         <a href={myResume} download>
           Resume
