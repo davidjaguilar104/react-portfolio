bca482d7867af3ffc6c76b88ce6d1ecbeb9463c4
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-16 13:05:39 -0600

    conditional render works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from './pages/About/About';
+import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Resume from "./pages/Resume/Resume";
 import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "#about") {
+      return <About />;
+    }
+    if (currentPage === "#portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "#contact") {
+      return <Contact />;
+    }
+    if (currentPage === "#resume") {
+      return <Resume/>;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <main>
-      <Header></Header>
-      <Portfolio></Portfolio>
+      <Header
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      ></Header>
+      {renderPage()}
       <Footer></Footer>
     </main>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 
-function Header() {
+function Header({ currentPage, handlePageChange }) {
   return (
     <nav>
       <h1 id="david-a">
@@ -9,16 +9,24 @@ function Header() {
       </h1>
       <div className="nav-items-wrapper">
         <h3>
-          <a href="#about">About Me</a>
+          <a 
+          onClick={() => handlePageChange('#about')}
+          href="#about">About Me</a>
         </h3>
         <h3>
-          <a href="#portfolio">Portfolio</a>
+          <a 
+          onClick={() => handlePageChange('#portfolio')}
+          href="#portfolio">Portfolio</a>
         </h3>
         <h3>
-          <a href="#contact">Contact</a>
+          <a 
+          onClick={() => handlePageChange('#contact')}
+          href="#contact">Contact</a>
         </h3>
         <h3>
-          <a href="#resume">Resume</a>
+          <a 
+          onClick={() => handlePageChange('#resume')}
+          href="#resume">Resume</a>
         </h3>
       </div>
     </nav>
