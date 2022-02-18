eb81d99f805d6725ebba21e10687c706617a6a73
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-18 11:02:29 -0600

    changed target to blank to avoid page change

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,10 +4,10 @@ import "./Project.css";
 const Project = ({ project }) => {
   return (
     <div className="link-img-wrapper">
-      <a href={project.repo} alt={project.repo}>
+      <a href={project.repo} alt={project.repo} target="blank">
         <h4 className="project-title">{project.name} Repository</h4>
       </a>
-      <a href={project.url}>
+      <a href={project.url} target="blank">
         <img src={project.image} alt={project.name} className="app-img" />
       </a>
     </div>
