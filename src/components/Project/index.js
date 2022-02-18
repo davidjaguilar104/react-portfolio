c0cfdef76dbcefa8f9085003a5af2a05fe1c0300
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-18 11:00:25 -0600

    implemented project comp with portfolio

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,109 +1,17 @@
 import React from "react";
-import animalShowdown from "../../assets/background.jpg";
-import recipeNutrition from "../../assets/stew-brew.png";
-import budgetTracker from "../../assets/budget-tracker.png";
-import weatherDash from "../../assets/weather-dashboard.png";
-import noteTaker from "../../assets/note-taker.png";
-import passGen from "../../assets/pass-gen.png";
+import "./Project.css";
 
-function Project() {
+const Project = ({ project }) => {
   return (
-    <section>
-      <h3>Portfolio</h3>
-      <div className="app-row">
-        <div className="link-img">
-          <a href="https://github.com/gstockha/animal-showdown">
-            Animal Showdown Repository
-          </a>
-          <a
-            href="https://animalshowdown.herokuapp.com/login"
-            alt="Link for Animal Showdown application"
-          >
-            <img
-              className="app-img"
-              src={animalShowdown}
-              alt="Animal Showdown app"
-            ></img>
-          </a>
-        </div>
-        <div className="link-img">
-          <a href="https://github.com/davidjaguilar104/recipe-nutrition-app">
-            Recipe Nutrition Repository
-          </a>
-          <a
-            href="https://davidjaguilar104.github.io/recipe-nutrition-app/"
-            alt="Link for Recipe Nutrition application"
-          >
-            <img
-              className="app-img"
-              src={recipeNutrition}
-              alt="Recipe Nutrition app"
-            ></img>
-          </a>
-        </div>
-      </div>
-      <div className="app-row">
-        <div className="link-img">
-          <a href="https://github.com/davidjaguilar104/pwa-budget-tracker">
-            Budget Tracker Repository
-          </a>
-          <a
-            href="https://github.com/davidjaguilar104/pwa-budget-tracker"
-            alt="Link for Budget Tracker application"
-          >
-            <img
-              className="app-img"
-              src={budgetTracker}
-              alt="Budget Tracker app"
-            ></img>
-          </a>
-        </div>
-        <div className="link-img">
-          <a href="https://github.com/davidjaguilar104/weather-dashboard">
-            Weather Dashboard Repository
-          </a>
-          <a
-            href="https://davidjaguilar104.github.io/weather-dashboard/"
-            alt="Link for Weather Dashboard application"
-          >
-            <img
-              className="app-img"
-              src={weatherDash}
-              alt="Weather Dashboard app"
-            ></img>
-          </a>
-        </div>
-      </div>
-      <div className="app-row">
-        <div className="link-img">
-          <a href="https://github.com/davidjaguilar104/note-taker">
-            Note Taker Repository
-          </a>
-          <a
-            href="https://glacial-coast-25788.herokuapp.com/"
-            alt="Link for Note Taker application"
-          >
-            <img className="app-img" src={noteTaker} alt="Note Taker app"></img>
-          </a>
-        </div>
-        <div className="link-img">
-          <a href="https://github.com/davidjaguilar104/password-generator">
-            Password Generator Repository
-          </a>
-          <a
-            href="https://davidjaguilar104.github.io/password-generator/"
-            alt="Link for Password Generator application"
-          >
-            <img
-              className="app-img"
-              src={passGen}
-              alt="Password Generator app"
-            ></img>
-          </a>
-        </div>
-      </div>
-    </section>
+    <div className="link-img-wrapper">
+      <a href={project.repo} alt={project.repo}>
+        <h4 className="project-title">{project.name} Repository</h4>
+      </a>
+      <a href={project.url}>
+        <img src={project.image} alt={project.name} className="app-img" />
+      </a>
+    </div>
   );
-}
+};
 
 export default Project;
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,11 +1,66 @@
 import React from "react";
-import Project from "../../components/Project";
 import "./Portfolio.css";
+import Project from "../../components/Project";
+import animalShowdown from "../../assets/background.jpg";
+import recipeNutrition from "../../assets/stew-brew.png";
+import budgetTracker from "../../assets/budget-tracker.png";
+import weatherDash from "../../assets/weather-dashboard.png";
+import noteTaker from "../../assets/note-taker.png";
+import passGen from "../../assets/pass-gen.png";
 
 function Portfolio() {
+  const projects = [
+    {
+      name: "Animal Showdown",
+      url: "https://animalshowdown.herokuapp.com/login",
+      image: animalShowdown,
+      repo: "https://github.com/gstockha/animal-showdown",
+    },
+    {
+      name: "Recipe Nutrition",
+      url: "https://davidjaguilar104.github.io/recipe-nutrition-app/",
+      image: recipeNutrition,
+      repo: "https://github.com/davidjaguilar104/recipe-nutrition-app",
+    },
+    {
+      name: "Budget Tracker",
+      url: "https://fast-taiga-23020.herokuapp.com/",
+      image: budgetTracker,
+      repo: "https://github.com/davidjaguilar104/pwa-budget-tracker",
+    },
+    {
+      name: "Weather Dashboard",
+      url: "https://davidjaguilar104.github.io/weather-dashboard/",
+      image: weatherDash,
+      repo: "https://github.com/davidjaguilar104/weather-dashboard",
+    },
+    {
+      name: "Note Taker",
+      url: "https://glacial-coast-25788.herokuapp.com/",
+      image: noteTaker,
+      repo: "https://github.com/davidjaguilar104/note-taker",
+    },
+    {
+      name: "Password Generator",
+      url: "https://davidjaguilar104.github.io/password-generator/",
+      image: passGen,
+      repo: "https://github.com/davidjaguilar104/password-generator",
+    },
+  ];
+
   return (
     <section>
-      <Project></Project>
+      <h3>Portfolio</h3>
+      <div className="app-row">
+        <Project project={projects[0]}></Project>
+        <Project project={projects[1]}></Project>
+        <Project project={projects[2]}></Project>
+      </div>
+      <div className="app-row">
+        <Project project={projects[3]}></Project>
+        <Project project={projects[4]}></Project>
+        <Project project={projects[5]}></Project>
+      </div>
     </section>
   );
 }
